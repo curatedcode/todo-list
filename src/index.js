a26e919aa3b6ca1ed5af76763f1e0eeb77d7c5ec
zack <zackaryfotheringham@hotmail.com>
2022-12-04 19:57:32 -0500

    check for clicks and trigger relevant functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import _ from 'lodash'
+import './style.css'
+import {OpenCloseSidebar} from './sidebar.js'
+import {OpenCloseAddTask} from './add-task.js'
+import {OpenCloseEditTask} from './edit-task.js'
+
+document.getElementsByClassName('open-sidebar')[0].addEventListener('click',()=>{OpenCloseSidebar('open')})
+document.getElementsByClassName('close-sidebar')[0].addEventListener('click',()=>{OpenCloseSidebar('close')})
+document.getElementsByClassName('add-task-btn')[0].addEventListener('click',()=>{OpenCloseAddTask('open')})
+document.getElementsByClassName('add-task-cancel')[0].addEventListener('click',()=>{OpenCloseAddTask('close')})
+document.getElementsByClassName('task-edit')[0].addEventListener('click',()=>{OpenCloseEditTask('open')})
+document.getElementsByClassName('edit-task-cancel')[0].addEventListener('click',()=>{OpenCloseEditTask('open')})
