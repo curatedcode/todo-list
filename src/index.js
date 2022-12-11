a1228d224f6c3e9417abc86986d3b75f2f57fd68
zack <zackaryfotheringham@hotmail.com>
2022-12-11 03:50:50 -0500

    listen and trigger relevant functions

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import './style.css'
 import { OpenCloseSidebar } from './sidebar.js'
 import { OpenCloseAddTask } from './add-task.js'
 import { OpenCloseEditTask } from './edit-task.js'
-import { UpdateDisplay } from './display.js'
+import { UpdateDisplay,AddProject } from './display.js'
 import { AddTask } from './add-task'
+
+UpdateDisplay('all')
 
 document.getElementsByClassName('open-sidebar')[0].addEventListener('click',()=>{OpenCloseSidebar('open')})
 document.getElementsByClassName('close-sidebar')[0].addEventListener('click',()=>{OpenCloseSidebar('close')})
 document.getElementsByClassName('add-task-btn')[0].addEventListener('click',()=>{OpenCloseAddTask('open')})
 document.getElementsByClassName('add-task-cancel')[0].addEventListener('click',()=>{OpenCloseAddTask('close')})
 document.getElementsByClassName('add-task-submit')[0].addEventListener('click',()=>{AddTask()})
-document.getElementsByClassName('task-edit')[0].addEventListener('click',()=>{OpenCloseEditTask('open')})
-document.getElementsByClassName('edit-task-cancel')[0].addEventListener('click',()=>{OpenCloseEditTask('close')})
-UpdateDisplay()
+document.getElementsByClassName('task-edit')[0].addEventListener('click',OpenCloseEditTask)
+document.getElementsByClassName('edit-task-cancel')[0].addEventListener('click',OpenCloseEditTask)
+document.getElementsByClassName('edit-task-submit')[0].addEventListener('click',OpenCloseEditTask)
+document.getElementsByClassName('add-project-btn')[0].addEventListener('click',AddProject)
+document.getElementsByClassName('default-filter')[0].addEventListener('click',()=>{window.location.reload()})
